4dc3d71996f46de3c297ad03209e906faf3cbdf1
Leeanh <leanhforevo@gmail.com>
2022-11-29 09:47:11 +0700

    Update Utils.java

--- android/src/main/java/com/imagepicker/Utils.java
+++ android/src/main/java/com/imagepicker/Utils.java
@@ -452,9 +452,9 @@ public class Utils {
                     uri = getAppSpecificStorageUri(uri, context);
                 }
                 uri = resizeImage(uri, context, options);
-                assets.pushMap(getImageResponseMap(uri, options, context));
+                assets.pushMap((WritableMap) getImageResponseMap(uri, options, context));
             } else if (isVideoType(uri, context)) {
-                assets.pushMap(getVideoResponseMap(uri, options, context));
+                assets.pushMap((WritableMap) getVideoResponseMap(uri, context));
             } else {
                 throw new RuntimeException("Unsupported file type");
             }
